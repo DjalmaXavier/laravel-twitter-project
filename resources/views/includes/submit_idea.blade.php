264cab55fdca3a9a5c8e40ab62d28072cb110ddb
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-07 20:47:38 -0300

    add submit_idea include

---
+++ resources/views/includes/submit_idea.blade.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="mb-3">
+        <form action="{{ route('idea.create') }}" method="POST">
+            @csrf
+            <textarea name="idea-content" class="form-control" id="idea" rows="3"></textarea>
+            @error('idea-content')
+                <span class="d-block m-2 fs-6 text-danger"> {{ $message }} </span>
+            @enderror
+    </div>
+    <div class="">
+        <button type="submit" class="btn btn-dark"> Share </button>
+    </div>
+    </form>
+</div>
