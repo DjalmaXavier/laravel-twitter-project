2df7dcf21b08153e70e5cbb0286f925186083357
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-07 20:41:33 -0300

    add the controller to the post route, for create and share the idea.
    add three validations (required, min and max)

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+    public function store()
+    {
+
+        $rules = [
+            'idea-content' => 'required|min:5|max:240',
+        ];
+        $message = [
+            'idea-content.required' => "The idea field is required.",
+            'idea-content.min' => "The idea field must be at least 5 characters.",
+            'idea-content.max' => "The idea field must not be greater than 240 characters."
+        ];
+        request()->validate($rules, $message);
+
+        try {
+            Idea::create([
+                'idea' => request()->get('idea-content', '')
+            ]);
+            return redirect()->route("dashboard")->with('sucess', 'Idea created!!');
+        } catch (\Throwable $th) {
+            return redirect()->route("dashboard")->with('error', 'Idea not created!!');
+        }
+    }
+}
