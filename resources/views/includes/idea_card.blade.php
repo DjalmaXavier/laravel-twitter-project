e0d10f7c01bf1d8b5e58efb5ae925762343195f9
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-10 23:59:36 -0300

    Add: idea update section

--- resources/views/includes/idea_card.blade.php
+++ resources/views/includes/idea_card.blade.php
@@ -14,6 +14,7 @@
                     <form action="{{ route('idea.destroy', $idea->id) }}" method="POST">
                         @csrf
                         @method('delete')
+                        <a class="mx-2" href="{{ route('idea.edit', $idea->id) }}"> Edit </a>
                         <a href="{{ route('idea.show', $idea->id) }}"> View </a>
                         <button class="ms-1 btn btn-danger btn-sm"> X </button>
                     </form>
@@ -22,9 +23,27 @@
             </div>
         </div>
         <div class="card-body">
-            <p class="fs-6 fw-light text-muted">
-                {{ $idea->idea }}
-            </p>
+            @if ($editing ?? false)
+                <div class="row">
+                    <div class="mb-3">
+                        <form action="{{ route('idea.update', $idea->id) }}" method="POST">
+                            @csrf
+                            @method('put')
+                            <textarea name="idea-content" class="form-control" id="idea" rows="3">{{ $idea->idea }}</textarea>
+                            @error('content')
+                                <span class="d-block m-2 fs-6 text-danger"> {{ $message }} </span>
+                            @enderror
+                    </div>
+                    <div class="">
+                        <button type="submit" class="btn btn-dark"> Share </button>
+                    </div>
+                    </form>
+                </div>
+            @else
+                <p class="fs-6 fw-light text-muted">
+                    {{ $idea->idea }}
+                </p>
+            @endif
             <div class="d-flex justify-content-between">
                 <div>
                     <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-heart me-1">
