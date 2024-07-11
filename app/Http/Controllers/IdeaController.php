368aa7015c1d3d0b5b2308aee75a062eb110b9b3
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-10 23:58:19 -0300

    Add: Edit controller, Update Controller Refactor: Compact used in Show controller

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -37,10 +37,33 @@ class IdeaController extends Controller
         return redirect()->route("dashboard")->with('sucess', 'Idea deleted!');;
     }
 
+    public function edit(Idea $idea)
+    {
+        $editing = true;
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+    public function update(Idea $idea)
+    {
+        $rules = [
+            'idea-content' => 'required|min:5|max:240',
+        ];
+        $message = [
+            'idea-content.required' => "The idea field is required.",
+            'idea-content.min' => "The idea field must be at least 5 characters.",
+            'idea-content.max' => "The idea field must not be greater than 240 characters."
+        ];
+        request()->validate($rules, $message);
+
+
+        $idea->idea = request()->get('idea-content', '');
+        $idea->save();
+        $editing = false;
+        return redirect()->route("idea.show", $idea->id)->with('sucess', "Idea updated!");
+    }
+
     public function show(Idea $idea)
     {
-        return view('ideas.show', [
-            'idea' => $idea
-        ]);
+        return view('ideas.show', compact('idea'));
     }
 }
