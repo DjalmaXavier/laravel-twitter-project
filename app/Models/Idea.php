8bec7d2dcea73413377b5d440cd28e67c9b36e71
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-15 23:58:12 -0300

    Add: Comment with the diference between filable and guarded

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -9,6 +9,11 @@ class Idea extends Model
 {
     use HasFactory;
 
+    /*
+    $fillable is the opposite of $guarded. When using $guarded, we specify the attributes that we do not want to be mass assignable. 
+    In contrast, with $fillable, we choose the attributes that we want to be mass assignable
+    */
+
     protected $fillable = [
         'idea',
         'likes'
