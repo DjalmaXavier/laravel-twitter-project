9a9e73384ecf4504f194d7a21ee1e0ea66cdcf75
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-19 23:21:37 -0300

    Update: Only a auth user can comment a post. Change the user name and the photo to match the owner of the comment.

--- resources/views/shared/comment-box.blade.php
+++ resources/views/shared/comment-box.blade.php
@@ -1,23 +1,32 @@
 <div>
-    <form action="{{ route('idea.comments.store', $idea->id) }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <textarea name='content'class="fs-6 form-control" rows="1"></textarea>
-        </div>
-        <div>
-            <button type="submit" class="btn btn-primary btn-sm"> Post Comment </button>
-        </div>
-    </form>
+    @auth
+        <form action="{{ route('idea.comments.store', $idea->id) }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <textarea name='content'class="fs-6 form-control" rows="1"></textarea>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary btn-sm"> Post Comment </button>
+            </div>
+        </form>
+    @endauth
+
+    @guest
+
+        <h4 class="mt-3">Login to comment!</h4>
+    @endguest
+
 
 
     @foreach ($idea->comments as $comment)
         <hr>
         <div class="d-flex align-items-start">
             <img style="width:35px" class="me-2 avatar-sm rounded-circle"
-                src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Luigi" alt="Luigi Avatar">
+                src="https://api.dicebear.com/6.x/fun-emoji/svg?seed={{ $comment->user->name }}"
+                alt="{{ $comment->user->name }}">
             <div class="w-100">
                 <div class="d-flex justify-content-between">
-                    <h6 class="">Luigi
+                    <h6 class="">{{ $comment->user->name }}
                     </h6>
                     <small class="fs-6 fw-light text-muted">Posted in {{ $comment->created_at }} </small>
                 </div>
