39e36cbb63bb77c5cab28b79a49b9642482aea3f
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-19 23:20:08 -0300

    Update: Now guesst can't post or comment any idea

--- resources/views/includes/submit_idea.blade.php
+++ resources/views/includes/submit_idea.blade.php
@@ -1,14 +1,20 @@
-<div class="row">
-    <div class="mb-3">
-        <form action="{{ route('idea.store') }}" method="POST">
-            @csrf
-            <textarea name="idea-content" class="form-control" id="idea" rows="3"></textarea>
-            @error('idea-content')
-                <span class="d-block m-2 fs-6 text-danger"> {{ $message }} </span>
-            @enderror
+@auth()
+    <h4> Share yours ideas </h4>
+    <div class="row">
+        <div class="mb-3">
+            <form action="{{ route('idea.store') }}" method="POST">
+                @csrf
+                <textarea name="idea-content" class="form-control" id="idea" rows="3"></textarea>
+                @error('idea-content')
+                    <span class="d-block m-2 fs-6 text-danger"> {{ $message }} </span>
+                @enderror
+        </div>
+        <div class="">
+            <button type="submit" class="btn btn-dark"> Share </button>
+        </div>
+        </form>
     </div>
-    <div class="">
-        <button type="submit" class="btn btn-dark"> Share </button>
-    </div>
-    </form>
-</div>
+@endauth
+@guest
+    <h4>Share your ideas!</h4>
+@endguest
