34c1eb4fa0376cbe3af1c10ea6a76313b3b45dc6
Djalma Xavier <djalmaxavier42@gmail.com>
2024-07-24 22:56:08 -0300

    Refactor: Use resource for more legibility and less code

--- routes/web.php
+++ routes/web.php
@@ -18,49 +18,15 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Comment;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group([
-    'prefix' => 'ideas',
-    'as' => 'idea.',
-], function () {
+Route::resource('ideas', IdeaController::class)->except(['index', 'create', 'show'])->middleware('auth');
 
-    Route::post(
-        '',
-        [IdeaController::class, 'store']
-    )->name('store');
+Route::resource('ideas', IdeaController::class)->only(['show']);
 
-    Route::get(
-        '/{idea}',
-        [IdeaController::class, 'show']
-    )->name('show');
-
-
-    Route::group([
-        'middleware' => ['auth']
-    ], function () {
-        Route::get(
-            '/{idea}/edit',
-            [IdeaController::class, 'edit']
-        )->name('edit');
-
-        Route::put(
-            '/{idea}',
-            [IdeaController::class, 'update']
-        )->name('update');
-
-        Route::delete(
-            '/{id}',
-            [IdeaController::class, 'destroy']
-        )->name('destroy');
-
-        Route::post(
-            '/ideas/{idea}/comments',
-            [CommentController::class, 'store']
-        )->name('comments.store');
-    });
-});
+Route::resource('ideas.comments', CommentController::class)->only(['store'])->middleware('auth');
 
 Route::get(
     '/',
