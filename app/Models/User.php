aaf0b3e0a93c0a0b0401442d8fa12e20077a40df
Djalma Xavier <djalmaxavier42@gmail.com>
2024-08-01 22:21:36 -0300

    Add: Relationship with followings and followers on user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,16 @@ class User extends Authenticatable
         return $this->hasMany(Comment::class)->latest();
     }
 
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'follower_user', 'follower_id', 'user_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'follower_user', 'user_id', 'follower_id')->withTimestamps();
+    }
+
     public function getImageUrl()
     {
         if ($this->image) {
