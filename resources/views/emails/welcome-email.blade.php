6ac12f07d103cc749d870aab450731e8c0b6920c
Djalma Xavier <djalmaxavier42@gmail.com>
2024-08-03 23:12:09 -0300

    Add: Welcome blade file to email

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome to Eureka!</title>
+</head>
+
+<body>
+    <h1>Welcome to Eureka, {{ $user->name }}!</h1>
+    <p>Thank you for joining our platform. We are excited to have you as part of our community.</p>
+    <p>You can start exploring and sharing your ideas right away!</p>
+    <p>If you have any questions, feel free to reach out to us.</p>
+    <p>Best regards,<br>
+        The Eureka Team</p>
+</body>
+
+</html>
