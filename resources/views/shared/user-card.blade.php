d327b2d15eabe4ededdaea05607f1da34add9775
Djalma Xavier <djalmaxavier42@gmail.com>
2024-08-03 23:40:26 -0300

    Add: If statemant for check if the person is following the user, if true, change the button to unfollow; Put a count to count the followers of the user.

--- resources/views/shared/user-card.blade.php
+++ resources/views/shared/user-card.blade.php
@@ -25,7 +25,7 @@
             </p>
             <div class="d-flex justify-content-start">
                 <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
-                    </span> 0 Followers </a>
+                    </span> {{ $user->followers->count() . ' Followers' }} </a>
                 <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-brain me-1">
                     </span> {{ $user->ideas()->count() }} </a>
                 <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-comment me-1">
@@ -33,12 +33,21 @@
             </div>
             @auth()
                 @if ($user->id != Auth::id())
-                    <div class="mt-3">
-                        <form action="{{ route('users.follow', $user->id) }}" method="POST">
-                            @csrf
-                            <button type="submit" tlass="btn btn-primary btn-sm"> Follow </button>
-                        </form>
-                    </div>
+                    @if (Auth::user()->follows($user))
+                        <div class="mt-3">
+                            <form action="{{ route('users.unfollow', $user->id) }}" method="POST">
+                                @csrf
+                                <button type="submit" class="btn btn-danger btn-sm"> Unfollow </button>
+                            </form>
+                        </div>
+                    @else
+                        <div class="mt-3">
+                            <form action="{{ route('users.follow', $user->id) }}" method="POST">
+                                @csrf
+                                <button type="submit" class="btn btn-primary btn-sm"> Follow </button>
+                            </form>
+                        </div>
+                    @endif
                 @endif
             @endauth
         </div>
