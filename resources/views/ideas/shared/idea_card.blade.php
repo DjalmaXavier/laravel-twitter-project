33117069b66f0e0cef4db566786552b9125d6473
Djalma Xavier <djalmaxavier42@gmail.com>
2024-08-15 23:18:53 -0300

    Update: Chamge the format of the time display and update the path of the comment box

--- resources/views/ideas/shared/idea_card.blade.php
+++ resources/views/ideas/shared/idea_card.blade.php
@@ -57,10 +57,10 @@
                 @include('ideas.shared.like-button')
                 <div>
                     <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
-                        {{ $idea->created_at }}</span>
+                        {{ $idea->created_at->diffForHumans() }}</span>
                 </div>
             </div>
-            @include('shared.comment-box')
+            @include('ideas.shared.comment-box')
         </div>
     </div>
 
